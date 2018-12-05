8782797be888cca712f4e5b4b127ae140e330ccb
Piotr Kozka <piotoor@gmail.com>
2018-12-05 20:19:39 +0100

    Initial commit

---
+++ Infrared_Line_Tracking_test.py
@@ -0,0 +1,9 @@
+import unittest
+import Infrared_Line_Tracking
+
+class Infrared_Line_Tracking_Test(unittest):
+    def testsdfasfasdf(self):
+        pass
+
+if __name__ == '__main__':
+    unittest.main()
---
+++ pid_test.py
@@ -0,0 +1,15 @@
+import unittest
+import pid
+
+class Pid_Test(unittest.TestCase):
+    def testCalculateDifference(self):
+        pidObj = pid.pid()
+        print(pidObj.calculateDifference(1000, 25, 100, 1000))
+
+    def testOnLine(self):
+        pidObj = pid.pid()
+        self.assertEqual(pidObj.calculateDifference(2000, 25, 100, 1000), 0)
+
+if __name__ == '__main__':
+    unittest.main()
+
