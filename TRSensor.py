e363d5b2736d0f5ba689bfb9e0c0050effc19f1b
SecT <tajnyt@gmail.com>
2019-01-18 12:46:21 +0100

    Removed local definition of state class

--- TRSensor.py
+++ TRSensor.py
@@ -1,14 +1,9 @@
 import RPi.GPIO as GPIO
-
+from States import state
 import collections
 
 
 state_history = collections.deque(maxlen=100)
-
-class state:
-    def __init__(self, state_name, sensor_values):
-        self.state_name = state_name
-        self.sensor_values = sensor_values
 
 
 class TRSensor(object):
