af112bfde507c145530cccb53321290e52f0aa27
Piotr Kozka <piotoor@gmail.com>
2019-02-19 19:37:29 +0100

    Implemented driving backwards

--- alphaBotSimulator/src/car.cpp
+++ alphaBotSimulator/src/car.cpp
@@ -9,6 +9,7 @@ car::car()
     sprite->setOrigin(assets::getTexture("car")->getSize().x / 2.0, assets::getTexture("car")->getSize().y / 2.0);
     sprite->setScale(0.1f, 0.1f);
     d = 4.2f;
+    dir = 1.0f;
 }
 
 car::~car()
@@ -20,17 +21,25 @@ void car::onKeyPressed()
 {
     if (sf::Keyboard::isKeyPressed(sf::Keyboard::Left))
     {
-        sprite->rotate(-1.6f);
+        sprite->rotate(-1.6f * dir);
     }
     if (sf::Keyboard::isKeyPressed(sf::Keyboard::Right))
     {
-        sprite->rotate(1.6f);
+        sprite->rotate(1.6f * dir);
     }
     if (sf::Keyboard::isKeyPressed(sf::Keyboard::Up))
     {
+        dir = 1.0f;
+        //sprite->move(d * sin(sprite->getRotation() * PI / 180.0), -d * cos(sprite->getRotation() * PI / 180.0));
         sprite->move(d * sin(sprite->getRotation() * PI / 180.0), -d * cos(sprite->getRotation() * PI / 180.0));
     }
-
+    if (sf::Keyboard::isKeyPressed(sf::Keyboard::Down))
+    {
+        d = -d;
+        dir = -1.0f;
+        sprite->move(d * sin(sprite->getRotation() * PI / 180.0), -d * cos(sprite->getRotation() * PI / 180.0));
+        d = -d;
+    }
 }
 
 sf::Sprite* car::getSprite()
