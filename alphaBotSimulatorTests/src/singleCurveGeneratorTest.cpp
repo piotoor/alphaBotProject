6dda19d01130200d71639f0ba3b14b7e2872a3e8
Piotr Kozka <piotoor@gmail.com>
2019-09-19 19:38:10 +0200

    Implemented triangleStrip test

--- alphaBotSimulatorTests/src/singleCurveGeneratorTest.cpp
+++ alphaBotSimulatorTests/src/singleCurveGeneratorTest.cpp
@@ -111,54 +111,49 @@ TEST(SingleCurveGenerator, Bezier2DTest2)
 
 TEST(SingleCurveGenerator, Bezier2DTriangleStrip)
 {
-    ASSERT_EQ(1, 0);
+    int segm = 2;
+    SingleCurveGenerator scg(segm,
+    {
+        make_pair(110.21, 210.324),
+        make_pair(10.23, 10.11),
+        make_pair(200.2, 110.0),
+        make_pair(230.33, 320.22),
+        make_pair(430.22, 573.75),
+        make_pair(830.22, 773.75),
+        make_pair(730.22, 473.75),
+        make_pair(430.22, 173.75)
+    });
+
+    vector<pair<double, double>> res = scg.Bezier2DTriangleStrip(15.0f);
+    cout << endl;
+    cout << "Bezier2D(): " << endl;
+    cout << "size = " << res.size() << endl;
+    cout << "segments = " << segm << endl;
+    for(size_t i = 0; i < res.size(); i++)
+    {
+        if(i % 5 == 0)
+        {
+            cout << endl;
+        }
+        cout << i << ":\t" << res[i].first << "; " << res[i].second << endl;
+    }
+
+    cout << endl;
+    cout << "------------------------------" << endl;
+    ASSERT_EQ(res.size(), 2 * (segm + 1));
+    vector<Point> expectedPoints
+    {
+        {110.21, 210.324},
+        {119.085, 198.231},
+        {394.388, 418.897},
+        {392.685, 403.994},
+        {430.22, 173.75},
+        {415.378, 171.581}
+    };
+    ASSERT_EQ(res.size(), expectedPoints.size());
+    for(size_t i = 0; i < expectedPoints.size(); i++)
+    {
+        ASSERT_LE(fabs(expectedPoints[i].first - res[i].first), epsilon);
+        ASSERT_LE(fabs(expectedPoints[i].second - res[i].second), epsilon);
+    }
 }
-//
-//TEST(SingleCurveGeneratorTest, dummyTest)
-//{
-//    int segm = 2;
-//    SingleCurveGenerator scg(segm,
-//    {
-//        make_pair(110.21, 210.324),
-//        make_pair(10.23, 10.11),
-//        make_pair(200.2, 110.0),
-//        make_pair(230.33, 320.22),
-//        make_pair(430.22, 573.75),
-//        make_pair(830.22, 773.75),
-//        make_pair(730.22, 473.75),
-//        make_pair(430.22, 173.75)
-//    });
-//
-//    vector<pair<double, double>> resStrip = scg.Bezier2DTriangleStrip(15.0f);
-//    cout << endl;
-//    cout << "Bezier2D(): " << endl;
-//    cout << "size = " << resStrip.size() << endl;
-//    cout << "segments = " << segm << endl;
-//    for(size_t i = 0; i < resStrip.size(); i++)
-//    {
-//        if(i % 5 == 0)
-//        {
-//            cout << endl;
-//        }
-//        cout << i << ":\t" << resStrip[i].first << "; " << resStrip[i].second << endl;
-//    }
-//
-//    cout << endl;
-//    cout << "------------------------------" << endl;
-//
-//    vector<pair<double, double>> res = scg.Bezier2D();
-//    cout << endl;
-//    cout << "Bezier2D(): " << endl;
-//    cout << "size = " << res.size() << endl;
-//    cout << "segments = " << segm << endl;
-//    for(size_t i = 0; i < res.size(); i++)
-//    {
-//        if(i % 5 == 0)
-//        {
-//            cout << endl;
-//        }
-//        cout << i << ":\t" << res[i].first << "; " << res[i].second << endl;
-//    }
-//
-//    ASSERT_EQ(1, 2);
-//}
