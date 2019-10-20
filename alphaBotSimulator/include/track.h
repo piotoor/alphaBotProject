6dea4deb554227a6c0318fe75ec4fd92662d4f77
Piotr Kozka <piotoor@gmail.com>
2019-10-20 12:18:31 +0200

    Implemented track class. Added track types; DEFAULT, RANDOM and DEFINED. Moved track generation from main to the track class.

--- alphaBotSimulator/include/track.h
+++ alphaBotSimulator/include/track.h
@@ -1,18 +1,34 @@
 #ifndef TRACK_H
 #define TRACK_H
-
+#include "SingleCurveGenerator.h"
 #include <SFML/Graphics.hpp>
 #include "assets.h"
+
+
 
 class track
 {
     public:
-        track();
+        enum trackType
+        {
+            DEFAULT,
+            RANDOM,
+            DEFINED
+        };
+
+    public:
+        track(trackType type = trackType::DEFAULT, const vector<Point> *controlPoints = nullptr);
         virtual ~track();
+        sf::VertexArray *getVertices();
 
     protected:
+        sf::VertexArray *vertexArray;
+        SingleCurveGenerator *scg;
 
     private:
+        vector<Point> trackPoints;
+
+
 };
 
 #endif // TRACK_H
--- alphaBotSimulator/src/track.cpp
+++ alphaBotSimulator/src/track.cpp
@@ -1,11 +1,41 @@
 #include "track.h"
 
-track::track()
+track::track(trackType type, const vector<Point> *controlPoints)
 {
-    //ctor
+    if(type == trackType::DEFAULT)
+    {
+        int segm = 80;
+        scg = new SingleCurveGenerator(segm,
+        {
+            make_pair(110.21, 210.324),
+            make_pair(10.23, 10.11),
+            make_pair(200.2, 110.0),
+            make_pair(230.33, 320.22),
+            make_pair(430.22, 573.75),
+            make_pair(830.22, 773.75),
+            make_pair(730.22, 473.75),
+            make_pair(430.22, 173.75)
+        });
+        trackPoints = scg-> Bezier2DTriangleStrip(15.0f);
+    }
+
+    vertexArray = new sf::VertexArray(sf::TriangleStrip, trackPoints.size());
+
+    for(size_t i = 0; i < trackPoints.size(); i++)
+    {
+        (*vertexArray)[i].position = sf::Vector2f(trackPoints[i].first, trackPoints[i].second);
+        (*vertexArray)[i].color = sf::Color::Red;
+    }
+
 }
 
 track::~track()
 {
-    //dtor
+    delete scg;
+    delete vertexArray;
+}
+
+sf::VertexArray* track::getVertices()
+{
+    return vertexArray;
 }
