9f562b2db39abdbddeae0e2c152fb572928b9a5f
SecT <tajnyt@gmail.com>
2020-02-10 20:06:08 +0100

    Added test for updatePWM

--- Simulator_test.py
+++ Simulator_test.py
@@ -1,5 +1,6 @@
 import unittest
 import Simulator as Simulator
+import AlphaBot
 
 class TRSensor_Test(unittest.TestCase):
     def test_dummy(self):
@@ -10,6 +11,10 @@ class TRSensor_Test(unittest.TestCase):
         vals = sim.getSensorValues()
         self.assertEqual(len(vals), 5)
 
+    def test_updatePWM(self):
+        ab = AlphaBot.AlphaBot()
+        sim = Simulator.Simulator()
+        sim.updatePWM(ab.PWMA, 50)
 
 if __name__ == '__main__':
     unittest.main()
